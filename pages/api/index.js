778d291d9b1fea9446f239a26239890d2edccf33
BELICIO CARDOSO <belloinfo@gmail.com>
2021-11-07 10:24:09 -0300

    criado um mockup com a API

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,14 +1,70 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+
   res.status(200).json(
-    [
-      { name: 'John Doe' },
-      { name: 'Belicio Cardoso' },
-      { name: 'Antonio Marcos' },
-      { name: 'Jose Maria' },
-      { name: 'Joâo Meneses' },
-      { name: 'Julia Posts' },
-    ]
+    {
+      drivethru: [
+        {
+          unidade: 'Uninassau',
+          endereco: 'Rua Dermeval de Souza Gusmão, Local 01-Quadra A- Faz. Grande III, sn, Salvador - BA, 41340-275',
+          atualizacao: '14:37:30',
+          dose: ["1ª DOSE", "2ª DOSE", "3ª DOSE"],
+          descrição: 'PRIMEIRA DOSE - PESSOAS COM 18 ANOS OU MAIS; SEGUNDA DOSE CORONAVAC PARA PESSOAS COM DATA DE REFORÇO NO CATÃO DE VACINA ATÉ O DIA 07 DE NOVEMBRO',
+        },
+        {
+          unidade: 'Cetro de Convenções',
+          endereco: 'Rua Dermeval de Souza Gusmão, Local 01-Quadra A- Faz. Grande III, sn, Salvador - BA, 41340-275',
+          atualizacao: '14:37:30',
+          dose: ["1ª DOSE", "2ª DOSE", "3ª DOSE"],
+          descrição: 'PRIMEIRA DOSE - PESSOAS COM 18 ANOS OU MAIS; SEGUNDA DOSE CORONAVAC PARA PESSOAS COM DATA DE REFORÇO NO CATÃO DE VACINA ATÉ O DIA 07 DE NOVEMBRO',
+        },
+        {
+          unidade: 'USF San Martin III',
+          endereco: 'Rua Dermeval de Souza Gusmão, Local 01-Quadra A- Faz. Grande III, sn, Salvador - BA, 41340-275',
+          atualizacao: '14:37:30',
+          dose: ["1ª DOSE", "2ª DOSE", "3ª DOSE"],
+          descrição: 'PRIMEIRA DOSE - PESSOAS COM 18 ANOS OU MAIS; SEGUNDA DOSE CORONAVAC PARA PESSOAS COM DATA DE REFORÇO NO CATÃO DE VACINA ATÉ O DIA 07 DE NOVEMBRO',
+        },
+        {
+          unidade: 'USF Estrada das Barreiras',
+          endereco: 'Rua Dermeval de Souza Gusmão, Local 01-Quadra A- Faz. Grande III, sn, Salvador - BA, 41340-275',
+          atualizacao: '14:37:30',
+          dose: ["1ª DOSE", "2ª DOSE", "3ª DOSE"],
+          descrição: 'PRIMEIRA DOSE - PESSOAS COM 18 ANOS OU MAIS; SEGUNDA DOSE CORONAVAC PARA PESSOAS COM DATA DE REFORÇO NO CATÃO DE VACINA ATÉ O DIA 07 DE NOVEMBRO',
+        }
+      ]
+      ,
+      Postosfixo: [
+        {
+          unidade: 'USF Fazenda Grande III',
+          endereco: 'Rua Dermeval de Souza Gusmão, Local 01-Quadra A- Faz. Grande III, sn, Salvador - BA, 41340-275',
+          atualizacao: '14:37:30',
+          dose: ["1ª DOSE", "2ª DOSE", "3ª DOSE"],
+          descrição: 'PRIMEIRA DOSE - PESSOAS COM 18 ANOS OU MAIS; SEGUNDA DOSE CORONAVAC PARA PESSOAS COM DATA DE REFORÇO NO CATÃO DE VACINA ATÉ O DIA 07 DE NOVEMBRO',
+        },
+        {
+          unidade: 'USF Joanes Leste',
+          endereco: 'Rua Dermeval de Souza Gusmão, Local 01-Quadra A- Faz. Grande III, sn, Salvador - BA, 41340-275',
+          atualizacao: '14:37:30',
+          dose: ["1ª DOSE", "2ª DOSE", "3ª DOSE"],
+          descrição: 'PRIMEIRA DOSE - PESSOAS COM 18 ANOS OU MAIS; SEGUNDA DOSE CORONAVAC PARA PESSOAS COM DATA DE REFORÇO NO CATÃO DE VACINA ATÉ O DIA 07 DE NOVEMBRO',
+        },
+        {
+          unidade: 'USF Itapuã',
+          endereco: 'Rua Dermeval de Souza Gusmão, Local 01-Quadra A- Faz. Grande III, sn, Salvador - BA, 41340-275',
+          atualizacao: '14:37:30',
+          dose: ["1ª DOSE", "2ª DOSE", "3ª DOSE"],
+          descrição: 'PRIMEIRA DOSE - PESSOAS COM 18 ANOS OU MAIS; SEGUNDA DOSE CORONAVAC PARA PESSOAS COM DATA DE REFORÇO NO CATÃO DE VACINA ATÉ O DIA 07 DE NOVEMBRO',
+        },
+        {
+          unidade: 'USF Estrada das Barreiras',
+          endereco: 'Rua Dermeval de Souza Gusmão, Local 01-Quadra A- Faz. Grande III, sn, Salvador - BA, 41340-275',
+          atualizacao: '14:37:30',
+          dose: ["1ª DOSE", "2ª DOSE", "3ª DOSE"],
+          descrição: 'PRIMEIRA DOSE - PESSOAS COM 18 ANOS OU MAIS; SEGUNDA DOSE CORONAVAC PARA PESSOAS COM DATA DE REFORÇO NO CATÃO DE VACINA ATÉ O DIA 07 DE NOVEMBRO',
+        }
+      ]
+    }
   )
 }
